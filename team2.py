2698418413abc5ebbd2cc058f145344dd2a7d60a
poon2610 <58822490+poon2610@users.noreply.github.com>
2020-01-29 10:20:21 -0600

    Update team2.py

--- team2.py
+++ team2.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Ok Pooner' # Only 10 chars displayed.
+strategy_name = 'Becollude'
+strategy_description = 'I_dont_know'
     
 def move(my_history, their_history, my_score, their_score):
     '''Make my move based on the history with this player.
