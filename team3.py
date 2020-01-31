aa56b0f43025d68f9aea32f04bd99f5158b6264b
Steven Reeves <58822375+StevenReeves23@users.noreply.github.com>
2020-01-31 10:40:59 -0600

    Update team3.py

--- team3.py
+++ team3.py
@@ -1,14 +1,8 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'Big Boi' # Only 10 chars displayed.
-strategy_name = 'BOOm'
-strategy_description = 'Grately'
+team_name = 'E3'
+strategy_name = 'Collude but retaliate'
+strategy_description = '''\
+Collude first round. Collude, except in a round after getting 
+a severe punishment.'''
     
 def move(my_history, their_history, my_score, their_score):
     '''Make my move based on the history with this player.
@@ -19,4 +13,10 @@ def move(my_history, their_history, my_score, their_score):
     The most recent round is my_history[-1] and their_history[-1]
     
     Returns 'c' or 'b' for collude or betray.
-    return 'b'
+    '''
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' # Betray if they were severely punished last time,
+    else:
+        return 'c' # otherwise collude.
